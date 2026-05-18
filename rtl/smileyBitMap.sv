605a849884583474e33f8e597f19029722622b4b
basharmousa138 <basharmousa138@gmail.com>
2026-05-18 14:46:10 +0300

    Update and rename smileyBitMap.sv to rtl/smileyBitMap.sv

--- rtl/smileyBitMap.sv
+++ rtl/smileyBitMap.sv
@@ -1,10 +1,4 @@
-// (c) Technion IIT, Department of Electrical Engineering 2021 
-//-- Alex Grinshpun Apr 2017
-//-- Dudy Nov 13 2017
-// SystemVerilog version Alex Grinshpun May 2018
-// coding convention dudy December 2018
 
-//-- Eyal Lev 31 Jan 2021
 
 module	objects_mux	(	
 //		--------	Clock Input	 	
